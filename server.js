07881276fd514ba6484e074f1a0a719ca9a92e05
Asa DeWeese <asadeweese@gmail.com>
2021-09-03 09:32:49 -0700

    Added date microservice

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,11 +19,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", (req, res) => {
+  var date;
+  var unix = parseInt(req.params.date);
+
+  if(!req.params.date){
+    let cur_unix = Date.now();
+    date = new Date(cur_unix);
+  } else if(isNaN(unix)) {
+    date = new Date(req.params.date);
+  } else {
+    date = new Date(unix);    
+  }
+
+  if(isNaN(date)) {
+    return res.json({error: 'Invalid Date'});
+  }
+
+  return res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
+
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
 
 
 
